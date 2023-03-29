8e04c6ca5dd590fe8899597007df469ecbf3994c
DreadPirateRoberts <46958197+ManfredRichthofen@users.noreply.github.com>
2023-03-29 09:52:10 -0700

    Formatting

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,56 +1,58 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
 export default defineConfig({
-  outDir: '../dist',
+  outDir: "../dist",
   title: "A3D",
   description: "A3D Wiki",
   cleanUrls: true,
   themeConfig: {
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'X1', link: '/sidewinder-x1/' },
-      { text: 'X2', link: '/sidewinder-x2/' },
-      { text: 'Genius', link: '/genius/' },
+      { text: "Home", link: "/" },
+      { text: "X1", link: "/sidewinder-x1/" },
+      { text: "X2", link: "/sidewinder-x2/" },
+      { text: "Genius", link: "/genius/" },
     ],
 
     sidebar: {
-
-      '/genius/': [
+      "/genius/": [
         {
-          text: 'Guide',
+          text: "Guide",
           items: [
-            { text: 'Firmware', link: '/genius/' },
-            { text: 'One', link: '/genius/Firmware' },
-            { text: 'Two', link: '/genius/two' }
-          ]
-        }
+            { text: "Firmware", link: "/genius/" },
+            { text: "One", link: "/genius/Firmware" },
+            { text: "Two", link: "/genius/two" },
+          ],
+        },
       ],
 
-      '/sidewinder-x1/': [
+      "/sidewinder-x1/": [
         {
-          text: 'Guide',
+          text: "Guide",
           items: [
-            { text: 'Firmware', link: '/sidewinder-x1/' },
-            { text: 'One', link: '/sidewinder-x1/firmware' },
-            { text: 'Two', link: '/sidewinder-x1/two' }
-          ]
-        }
+            { text: "Firmware", link: "/sidewinder-x1/" },
+            { text: "One", link: "/sidewinder-x1/firmware" },
+            { text: "Two", link: "/sidewinder-x1/two" },
+          ],
+        },
       ],
 
-      '/sidewinder-x2/': [
+      "/sidewinder-x2/": [
         {
-          text: 'Config',
+          text: "Config",
           items: [
-            { text: 'Index', link: '/sidewinder-x2/' },
-            { text: 'Three', link: '/sidewinder-x2/three' },
-            { text: 'Four', link: '/sidewinder-x2/four' }
-          ]
-        }
-      ]
+            { text: "Index", link: "/sidewinder-x2/" },
+            { text: "Three", link: "/sidewinder-x2/three" },
+            { text: "Four", link: "/sidewinder-x2/four" },
+          ],
+        },
+      ],
     },
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/ManfredRichthofen/Artillery-Docs' },
-    ]
-  }
-})
+      {
+        icon: "github",
+        link: "https://github.com/ManfredRichthofen/Artillery-Docs",
+      },
+    ],
+  },
+});
